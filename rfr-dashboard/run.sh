ee892d868382728bd100d549b8268deb09f5241c
Matthew Vandenberg <mv5903@gmail.com>
2023-02-07 22:20:15 -0500

    start bash script

--- rfr-dashboard/run.sh
+++ rfr-dashboard/run.sh
@@ -1,1 +1,25 @@
-npm install && npm run start & nodemon backend/src/index.ts
+#npm run start & nodemon backend/src/index.ts
+echo "Starting rfr-dashboard..."
+clear
+# Check Node Version
+echo "Checking if Node.js is installed..."
+if type node > /dev/null 2>&1 && which node > /dev/null 2>&1 ;then
+    VERSION_OUTPUT="$(node -v)"
+    OUTPUT="$(echo $VERSION_OUTPUT | cut -d'v' -f2)"
+    MAJOR="$(echo $OUTPUT | cut -d'.' -f1)"
+    if [ $MAJOR -ge 18 ]; then
+        echo "Node.js is installed and is at least version 18, continuing..."
+    else
+        echo "Node.js is installed but is not at least version 18, attempting to install latest version..."
+        npm install -g npm
+    fi
+else
+    echo "Node.js is not installed or has not been detected. Please install node and try again."
+    exit 1
+fi
+# Install all dependencies
+echo "Installing dependencies, if they don't already exist..."
+echo "Running npm install..."
+npm install
+echo "Running npm install -g nodemon..."
+npm install -g nodemon
